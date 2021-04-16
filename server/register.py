8c9527fd95f43123b7e743c52c46d1b20406bee7
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2021-04-16 12:07:59 +0800

    Implement build flows

--- server/register.py
+++ server/register.py
@@ -1,10 +1,15 @@
 """Registers flows to be managed by the Prefect Backend.
 """
 
+import logging
+
 from configparser import ConfigParser
+from typing import List
+from prefect import Flow
 
 from prefect.storage import Docker
 from prefect.executors import DaskExecutor
+from prefect.executors import LocalExecutor
 
 from prefect.engine.results import AzureResult
 from prefect.engine.results import S3Result
@@ -14,24 +19,26 @@ from server.src.flows.data import e2e_pipeline
 from server.src.flows.mock import mapreduce_wordcount
 
 
+# CONFIGS
+
 # Get configs
 config = ConfigParser(allow_no_value=True)
 config.read('configs/pipeline.ini')
 
 # Project name
-PROJECT_NAME = config.get('prefect', 'PROJECT_NAME')
+PROJECT_NAME = config.get('prefect.default', 'PROJECT_NAME')
 # URL to Docker registry to store Flow scripts
-REGISTRY_URL = config.get('prefect', 'REGISTRY_URL')
+REGISTRY_URL = config.get('prefect.storage', 'REGISTRY_URL')
 # Dask scheduler address to be used in DaskExecutor
-DASK_SCHEDULER_ADDR = config.get('prefect', 'DASK_SCHEDULER_ADDR')
+DASK_SCHEDULER_ADDR = config.get('prefect.executor', 'DASK_SCHEDULER_ADDR')
 # Configs for Result subclass
-RESULT_SUBCLASS = config.get('prefect', 'RESULT_SUBCLASS')
-AZURE_RESULT_CONTAINER = config.get('prefect', 'AZURE_RESULT_CONTAINER')
-S3_RESULT_BUCKET = config.get('prefect', 'S3_RESULT_BUCKET')
-LOCAL_RESULT_DIR = config.get('prefect', 'LOCAL_RESULT_DIR')
+RESULT_SUBCLASS = config.get('prefect.result', 'RESULT_SUBCLASS')
+AZURE_RESULT_CONTAINER = config.get('prefect.result', 'AZURE_RESULT_CONTAINER')
+S3_RESULT_BUCKET = config.get('prefect.result', 'S3_RESULT_BUCKET')
+LOCAL_RESULT_DIR = config.get('prefect.result', 'LOCAL_RESULT_DIR')
 
 
-# Prefect configs
+# FLOWS CONFIGURATION
 
 # Get python requirements
 with open('./requirements/requirements-flow.txt') as f:
@@ -47,7 +54,8 @@ storage_kwargs = {
 storage = Docker(**storage_kwargs)
 
 # Executer
-executor = DaskExecutor(address=DASK_SCHEDULER_ADDR)
+dask_executor = DaskExecutor(address=DASK_SCHEDULER_ADDR)
+local_executor = LocalExecutor()
 
 # Result
 if RESULT_SUBCLASS == 'azure':
@@ -58,20 +66,38 @@ else:
     result = LocalResult(dir=LOCAL_RESULT_DIR)
 
 
-# Set Flow storage
+# Set flow storage
 e2e_pipeline.storage = storage
 mapreduce_wordcount.storage = storage
 
 
-# Set Flow Executer
-e2e_pipeline.executor = executor
-mapreduce_wordcount.executor = executor
+# Set flow executer
+e2e_pipeline.executor = local_executor
+mapreduce_wordcount.executor = dask_executor
 
 
-# Set Flow Result
+# Set flow result
 e2e_pipeline.result = result
 mapreduce_wordcount.result = result
 
 
+# Declare flows
+FLOWS = [
+    e2e_pipeline,
+    mapreduce_wordcount
+]
+
+
+# Build flows
+def build_flows(flows: List[Flow],
+                project_name: str = PROJECT_NAME):
+    for flow in flows:
+        logging.info(flow.name)
+        flow.validate()
+        flow.register(project_name=project_name,
+                      idempotency_key=flow.serialized_hash())
+
+
 if __name__ == "__main__":
-    pass
+    logging.basicConfig(level=logging.INFO)
+    build_flows(flows=FLOWS)
