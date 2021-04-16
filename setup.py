ce4767e3ab8b539de1d401ef7742502c073a6b9c
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2021-04-16 20:44:23 +0800

    Transition from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,43 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages
-from setuptools import setup
+import setuptools
 
-
-with open("README.md") as f:
-    long_description = f.read()
-
-
-requirements_files = [
-    'requirements-server.txt',
-    'requirements-client.txt',
-    'requirements-flow.txt',
-]
-
-requirements = []
-for f in requirements_files:
-    with open(f"requirements/{f}") as f:
-        requirements += [line for line in f.readlines()]
-
-with open("requirements/requirements-test.txt") as f:
-    test_requirements = [line for line in f.readlines()]
-
-extras = {
-    "dev": ["flake8"],
-    "test": test_requirements
-}
-
-setup(
-    author="Christopher Lo",
-    author_email="lochristopherhy@gmail.com",
-    python_requires="==3.8.*",
-    description="Boilerplate for an end-to-end data app.",
-    install_requires=requirements,
-    extras_require=extras,
-    long_description=long_description,
-    include_package_data=True,
-    keywords="datathon-mlapp-starter",
-    name="datathon-mlapp-starter",
-    url="https://github.com/topher-lo/datathon-mlapp-starter",
-    version="0.0.1",
-)
+if __name__ == "__main__":
+    setuptools.setup()
