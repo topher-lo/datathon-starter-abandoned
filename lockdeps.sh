32c7b5d3548fecf07c6e7dd9c797afe92c97d33b
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2021-04-18 21:57:24 +0800

    Constrain requirements using -c in requirements.in files

---
+++ lockdeps.sh
@@ -0,0 +1,4 @@
+pip-compile requirements/server-requirements.in
+pip-compile requirements/flows-requirements.in
+pip-compile requirements/client-requirements.in
+pip-compile requirements/dev-requirements.in
