10774bee059c261ebe4cfe6817ad4cb858c3af0e
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2021-04-19 17:20:48 +0800

    Add bash script to install deps

---
+++ installdeps.sh
@@ -0,0 +1,4 @@
+pip3 install -r requirements/server-requirements.txt
+pip3 install -r requirements/flows-requirements.txt
+pip3 install -r requirements/client-requirements.txt
+pip3 install -r requirements/dev-requirements.txt
